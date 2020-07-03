69eb4c2d79db2d815089d36345f085b5930af5b0
Expedito Andrade <expeditojazz@gmail.com>
2020-07-03 12:29:10 -0300

    initializing development

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const home = () => {
+  return (
+    <div>
+      Hello from the home page
+    </div>
+  )
+}
---
+++ src/pages/Rooms.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Rooms = () => {
+  return (
+    <div>
+      Hello from the rooms page
+    </div>
+  )
+}
