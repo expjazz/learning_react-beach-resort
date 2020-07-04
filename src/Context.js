a12e3c3d62f3b32338b1443845339c146bd055d9
Expedito Andrade <expeditojazz@gmail.com>
2020-07-04 16:52:40 -0300

    add data from dataJs to context

--- src/Context.js
+++ src/Context.js
@@ -21,7 +21,12 @@ class RoomProvider extends Component {
   }
   componentDidMount() {
     let rooms = this.formatData(items);
-    console.log(rooms)
+    let featuredRooms = rooms.filter(room => room.featured === true);
+    this.setState({
+      rooms, featuredRooms, 
+      sortedRooms: rooms, loading: false
+
+    })
   }
 
   
--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import {RoomContext} from '../Context'
+import Loading from './Loading'
+import Room from './Room'
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext
   render() {
+    const { featuredRooms : rooms}  = this.context
+    console.log(rooms)
     return (
       <div>
         Hello from feature rooms
+        <Room />
+        <Loading />
       </div>
     )
   }
