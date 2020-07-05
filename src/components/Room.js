50db5adb69830a2550f397168c97b8878d6c4ecc
Expedito Andrade <expeditojazz@gmail.com>
2020-07-05 14:29:41 -0300

    add room component

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import defaultImg from '../images/room-1.jpeg'
+export default function Room({room}) {
+  const {name, slug, images, price} = room;
 
-export default function Room() {
   return (
-    <div>
-      Hello from room
-    </div>
+    <article className="room">
+      <div className="img-container">
+        <img src={ images[0] || defaultImg } alt="singleroom"/>
+        <div className="price-top">
+          <h6>
+            ${price}
+          </h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className='btn-primary room-link'> Features </Link>
+      </div>
+    </article>
   )
 }
